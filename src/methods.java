70dac540fe8411f15ffc5820af692a6f953414e1
Conor Barry <conor_barry@icloud.com>
2020-05-17 16:13:45 +0100

    Fixed validation issue where no error was displayed when the player's name was empty

--- src/methods.java
+++ src/methods.java
@@ -14,7 +14,7 @@ class methods {
         for (int i = 0; i < numPlayers; i++) {
             while (true) {
                 System.out.print("Enter player " + (i + 1) + "'s name: ");
-                player[i] = keyboard.next();
+                player[i] = keyboard.nextLine();
 
                 // if the user didn't enter a player name, inform them that the player name cannot be empty
                 if (player[i] == null || player[i].length() == 0) {
@@ -32,7 +32,7 @@ class methods {
         int score = 0;
         String didPlay;
         System.out.print("\nDid " + player.getName() + " play in the game on week " + playerWeek + "? (y/n) ");
-        didPlay = keyboard.next();
+        didPlay = keyboard.nextLine();
 
         // Check if the player played in the game
         // if they did award them 2 points and proceed to ask further details about the players scores
@@ -42,24 +42,27 @@ class methods {
             int didScore;
             System.out.print("How many times did " + player.getName() + " score a goal in the game? ");
             didScore = keyboard.nextInt();
+            keyboard.nextLine();
             // Award 5 points for scoring each goal
             score += 5 * didScore;
 
             int goalAssist;
             System.out.print("How many times did " + player.getName() + " assist a goal? ");
             goalAssist = keyboard.nextInt();
+            keyboard.nextLine();
             // Award 3 points for assisting each goal
             score += 3 * goalAssist;
 
             int missedPenalty;
             System.out.print("How many times did " + player.getName() + " miss a penalty? ");
             missedPenalty = keyboard.nextInt();
+            keyboard.nextLine();
             //subtract 3 points for each missed penalty
             score -= 3 * missedPenalty;
 
             String yellowCard;
             System.out.print("Did " + player.getName() + " get a yellow card? (y/n) ");
-            yellowCard = keyboard.next();
+            yellowCard = keyboard.nextLine();
             if (yellowCard.equals("y")) {
                 //subtract 1 point for a yellow card
                 score -= 1;
@@ -67,7 +70,7 @@ class methods {
 
             String redCard;
             System.out.print("Did " + player.getName() + " get a red card? (y/n) ");
-            redCard = keyboard.next();
+            redCard = keyboard.nextLine();
             if (redCard.equals("y")) {
                 //subtract 3 points for a red card
                 score -= 3;
@@ -75,7 +78,7 @@ class methods {
 
             String manOfMatch;
             System.out.print("Was " + player.getName() + " the man of the match? (y/n) ");
-            manOfMatch = keyboard.next();
+            manOfMatch = keyboard.nextLine();
             if (manOfMatch.equals("y")) {
                 // Award 5 points for the man of the match
                 score += 5;
